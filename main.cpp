b20fb261814621544f90882acb442c769833b592
Nifemi <nifemiolayiwola49@gmail.com>
2026-07-22 22:35:00 -0500

    Refactor main function: add student management options and improve user interaction

--- main.cpp
+++ main.cpp
@@ -1,5 +1,54 @@
 #include <iostream>
 using namespace std;
-int main{
-    
+
+void addStudent();
+void viewStudent();
+void searchStudent();
+void updateStudent();
+void deleteStudent();
+
+int main
+{
+    int choice;
+
+    do
+    {
+        cout << "Student Management System" << endl;
+        cout << "1. Add Student" << endl;
+        cout << "2. View Students" << endl;
+        cout << "3. Search Student" << endl;
+        cout << "4. Update Student" << endl;
+        cout << "5. Delete Student" << endl;
+        cout << "6. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                viewStudent();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                updateStudent();
+                break;
+            case 5:
+                deleteStudent();
+                break;
+            case 6:
+                cout << "Student data saved to file." << endl;
+            case 7:
+                cout << "Exiting the program." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 7);
+
+    return 0;
 }
